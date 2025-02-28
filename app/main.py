d51f922e073112c105772084acd9e4f14a520d9a
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2025-02-28 17:22:20 -0300

    script limpado e arrumado para deixar os csvs mais legíveis

--- app/main.py
+++ app/main.py
@@ -10,37 +10,37 @@ conexaoOBD = obd.OBD('com4')
 now = datetime.now()
 str_time = now.strftime('%d-%m-%Y_%H-%M-%S')
 
-rotacoesCmd = obd.commands.RPM
-velocidadeCmd = obd.commands.SPEED
-temperaturaCmd = obd.commands.COOLANT_TEMP
-combustivelCmd = obd.commands.FUEL_LEVEL
-
-with open ('../../data/data_'+str_time+'.csv', 'w', newline='') as csvfile:
+with open ('../data/data_'+str_time+'.csv', 'w', newline='') as csvfile:
     fieldnames = ['time', 'rpm', 'velocidade', 'temperatura', 'combustivel']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel')
     
     writer.writeheader()
 
     while True:
-        rotacoesRec = conexaoOBD.query(rotacoesCmd)
-        velocidadeRec = conexaoOBD.query(velocidadeCmd)
-        temperaturaRec = conexaoOBD.query(temperaturaCmd)
-        combustivelRec = conexaoOBD.query(combustivelCmd)
+        rpm_res = conexaoOBD.query(obd.commands.RPM)
+        speed_res = conexaoOBD.query(obd.commands.SPEED)
+        temp_res = conexaoOBD.query(obd.commands.COOLANT_TEMP)
+        fuel_res = conexaoOBD.query(obd.commands.FUEL_LEVEL)
+        
+        rpm_num = str(rpm_res.value.magnitude)
+        speed_num = str(speed_res.value.magnitude)
+        temp_num = str(temp_res.value.magnitude)
+        fuel_num = str(fuel_res.value.magnitude)
 
-        print("RPM: " + str(rotacoesRec.magnitude))
-        print("Velocidade: " + str(velocidadeRec.magnitude))
-        print("Temperatura: " + str(temperaturaRec.magnitude))
-        print("Combustivel: " + str(combustivelRec.magnitude))
+        print("RPM: " + rpm_num)
+        print("Velocidade: " + speed_num)
+        print("Temperatura: " + temp_num)
+        print("Combustivel: " + fuel_num)
         
         now = datetime.now();
         str_time = now.strftime("%H-%M-%S")
         
         writer.writerow({
             'time': str_time,
-            'rpm': str(rotacoesRec.magnitude), 
-            'velocidade': str(velocidadeRec.magnitude), 
-            'temperatura': str(temperaturaRec.magnitude), 
-            'combustivel': str(combustivelRec.magnitude)
+            'rpm': rpm_num, 
+            'velocidade': speed_num, 
+            'temperatura': temp_num, 
+            'combustivel': fuel_num
             })
 
         time.sleep(1)
