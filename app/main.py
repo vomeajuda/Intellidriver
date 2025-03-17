6956ebafebf1b7e302da8f00f75874266c503259
vomeajuda <leogugo9@hotmail.com>
2025-03-17 19:58:14 -0300

    adição de try para erro

--- app/main.py
+++ app/main.py
@@ -26,18 +26,21 @@ with open ('../data/data_'+str_time+'.csv', 'w', newline='') as csvfile:
         absload_res = conexaoOBD.query(obd.commands.ABSOLUTE_LOAD)
         type_res = conexaoOBD.query(obd.commands.FUEL_TYPE)
         oil_res = conexaoOBD.query(obd.commands.OIL_TEMP)
-        # rate_res = conexaoOBD.query(obd.commands.FUEL_RATE)
+        rate_res = conexaoOBD.query(obd.commands.FUEL_RATE)
         
         rpm_num = str(rpm_res.value.magnitude)
         speed_num = str(speed_res.value.magnitude)
         temp_num = str(temp_res.value.magnitude)
         fuel_num = str(round(fuel_res.value.magnitude, 3))
         load_num = str(round(load_res.value.magnitude, 3))
-        throttle_num = str(round(throttle_res.value.magnitude)
-        absload_num = str(absload_res.value.magnitude, 3))
+        throttle_num = str(round(throttle_res.value.magnitude))
+        absload_num = str(absload_res.value.magnitude, 3)
         type_num = str(type_res.value)
         oil_num = str(oil_res.value.magnitude)
-        # rate_num = str(rate_res.value.magnitude)
+        try:
+            rate_num = str(rate_res.value.magnitude)
+        except:
+            rate_num = 0
 
         print("RPM: " + rpm_num)
         print("Velocidade: " + speed_num)
@@ -48,7 +51,7 @@ with open ('../data/data_'+str_time+'.csv', 'w', newline='') as csvfile:
         print("Absolute Load: " + absload_num)
         print("Tipo Combustivel: " + type_num)
         print("Temperatura Oleo: " + oil_num)
-        # print("L/h: " + rate_num)
+        print("L/h: " + rate_num)
         
         now = datetime.now();
         str_time = now.strftime("%H-%M-%S")
@@ -64,7 +67,7 @@ with open ('../data/data_'+str_time+'.csv', 'w', newline='') as csvfile:
             'Absolute_Load': absload_num,
             'Tipo_Combustivel': type_res,
             'Temperatura_Oleo': oil_num,
-            # 'L/h ': rate_num
+            'L/h ': rate_num
             })
 
         time.sleep(1)
