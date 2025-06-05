99e57383361dadafc3b81f47f23246ae2fba1b88
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2025-06-05 15:13:18 -0300

    navbar finalmente feita (sem funcionalidade)

--- web_app/src/components/Header/Header.jsx
+++ web_app/src/components/Header/Header.jsx
@@ -1,37 +1,30 @@
-import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 import { logo } from '../../assets';
 
 const Header = () => {
   return (
     <Navbar key={'md'} expand={'md'} fixed='top' className='bg-secondary text-primary'>
-      <Container className='d-flex flex-wrap justify-content-center align-items-end'>
-          <Navbar.Brand className='d-flex align-items-center me-auto me-md-0 me-lg-auto'>
-            <img className='me-3 ms-md-3' height={150} src={logo} alt='Logo do IncluMove' />
-          </Navbar.Brand>
+      <Container className='d-flex flex-wrap justify-content-center align-items-center align-items-md-end'>
+        <Navbar.Brand className='d-flex align-items-center me-auto me-md-0'>
+          <img className='me-3 ms-md-3' height={150} src={logo} alt='Logo do IncluMove' />
+        </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls='offcanvasNavbar-expand-md' />
-        <Navbar.Offcanvas
-          className='bg-secondary text-primary d-md-none'
-          id='offcanvasNavbar-expand-md'
-          aria-labelledby='offcanvasNavbarLabel-expand-md'
-          placement="end"
+        <Navbar.Toggle className='py-2 me-3' aria-controls='collapseNavbar-expand-md' />
+
+        <Navbar.Collapse
+          className='bg-secondary text-primary d-md-none justify-content-center justify-content-lg-end'
+          id='collapseNavbar-expand-md'
+          aria-labelledby='collapseNavbarLabel-expand-md'
         >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id='offcanvasNavbarLabel-expand-md'>
-              IntelliDriver
-            </Offcanvas.Title>
-          </Offcanvas.Header>
-        </Navbar.Offcanvas>
-
-        <Nav defaultActiveKey='/' className='text-primary fs-3 fw-semibold montserrat gap-xl-4 d-none d-md-flex'>
-
-          <Nav.Link>Início</Nav.Link>
-          <Nav.Link>Ranking</Nav.Link>
-          <Nav.Link>Objetivo</Nav.Link>
-          <Nav.Link>Sobre Nós</Nav.Link>
-          <Nav.Link>Contato</Nav.Link>
-        </Nav>
+          <Nav defaultActiveKey='/' className='gap-xl-4 d-md-flex text-primary fs-3 fw-semibold montserrat ps-3 ps-md-0'>
+            <Nav.Link>Início</Nav.Link>
+            <Nav.Link>Ranking</Nav.Link>
+            <Nav.Link>Objetivo</Nav.Link>
+            <Nav.Link>Sobre Nós</Nav.Link>
+            <Nav.Link>Contato</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
