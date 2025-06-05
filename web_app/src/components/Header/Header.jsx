a3cfb0dc5325ed03019365aa246886c2f35c1cad
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2025-06-04 21:25:43 -0300

    navbar quase feita

--- web_app/src/components/Header/Header.jsx
+++ web_app/src/components/Header/Header.jsx
@@ -1,37 +1,41 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
 
 import { logo } from '../../assets';
 
 const Header = () => {
   return (
-    <Navbar fixed='top' className='bg-secondary text-primary'>
-      <Container>
-        <header className='d-flex flex-wrap justify-content-center align-items-end w-100'>
-          <a className='d-flex align-items-center mb-3 mb-md-0 me-0 me-lg-auto' href="/">
+    <Navbar key={'md'} expand={'md'} fixed='top' className='bg-secondary text-primary'>
+      <Container className='d-flex flex-wrap justify-content-center align-items-end'>
+          <Navbar.Brand className='d-flex align-items-center me-auto me-md-0 me-lg-auto'>
             <img className='me-3 ms-md-3' height={150} src={logo} alt='Logo do IncluMove' />
-          </a>
+          </Navbar.Brand>
 
-          <Nav defaultActiveKey='/' as='ul' className='text-primary fs-3 fw-semibold montserrat gap-xl-4'>
-            <Nav.Item as='li'>
-              <Nav.Link>Início</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as='li'>
-              <Nav.Link>Ranking</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as='li'>
-              <Nav.Link>Objetivo</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as='li'>
-              <Nav.Link>Sobre Nós</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as='li'>
-              <Nav.Link>Contato</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </header>
+        <Navbar.Toggle aria-controls='offcanvasNavbar-expand-md' />
+        <Navbar.Offcanvas
+          className='bg-secondary text-primary d-md-none'
+          id='offcanvasNavbar-expand-md'
+          aria-labelledby='offcanvasNavbarLabel-expand-md'
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id='offcanvasNavbarLabel-expand-md'>
+              IntelliDriver
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+        </Navbar.Offcanvas>
+
+        <Nav defaultActiveKey='/' className='text-primary fs-3 fw-semibold montserrat gap-xl-4 d-none d-md-flex'>
+
+          <Nav.Link>Início</Nav.Link>
+          <Nav.Link>Ranking</Nav.Link>
+          <Nav.Link>Objetivo</Nav.Link>
+          <Nav.Link>Sobre Nós</Nav.Link>
+          <Nav.Link>Contato</Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
 }
 
 export { Header };
+
