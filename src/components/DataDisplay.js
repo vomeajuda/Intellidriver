bed7199503bbd96720394fd045e3f28a0a3b0a83
vomeajuda <leogugo9@hotmail.com>
2025-09-26 17:44:44 -0300

    a little visual editing

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -1,11 +1,12 @@
 import { View, Text } from "react-native";
+import { colors } from '../constants/theme';
 
 const DataDisplay = ({ rpm, speed, coolant, fuel }) => (
-  <View style={{ marginTop: 20, padding: 15, backgroundColor: "#111", borderRadius: 12 }}>
-    {rpm !== null && <Text style={{ color: "cyan" }}>RPM: {rpm}</Text>}
-    {speed !== null && <Text style={{ color: "cyan" }}>Velocidade: {speed} km/h</Text>}
-    {coolant !== null && <Text style={{ color: "cyan" }}>Temp: {coolant} °C</Text>}
-    {fuel !== null && <Text style={{ color: "cyan" }}>Combustível: {fuel}%</Text>}
+  <View style={{ marginTop: 20, padding: 15, backgroundColor: colors.surface, borderRadius: 12 }}>
+    {rpm !== null && <Text>RPM: {rpm}</Text>}
+    {speed !== null && <Text>Velocidade: {speed} km/h</Text>}
+    {coolant !== null && <Text>Temp: {coolant} °C</Text>}
+    {fuel !== null && <Text>Combustível: {fuel}%</Text>}
   </View>
 );
 
